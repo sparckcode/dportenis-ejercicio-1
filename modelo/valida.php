9f06eb0bfebc8f674dcd394f784c3a344ab94e84
Marcos Ortega <marcos.ortega@dportenis.com.mx>
2023-10-12 16:50:39 -0700

    (dev1) - FIXED: Class valida is adjusted and corrected to return array object, add docto.

--- modelo/valida.php
+++ modelo/valida.php
@@ -1,34 +1,87 @@
 <?php  
+
+/**
+ * Calss valida
+ * 
+ * @author Marcos Ortega <marcos.ortega@dportenis.com.mx>
+ */
+class valida
+{
+	private $response = array();
+	private $rutatemp = "temp/";
+
 	/**
+	 * Create a response.
 	 * 
+	 * @param int $codigo
+	 * @param string $mensaje
+	 * @param array $objeto
+	 * 
+	 * @return void
 	 */
-	class valida
+	public function CreaRespuesta($codigo, $mensaje = "", $objeto = null): void
 	{
-		private $response = array();
-		private $rutatemp = "temp/";
-		public function __construct()
-		{			
-		}
-		public function CreaRespuesta($codigo, $mensaje = "", $objeto = null){
-			switch ($codigo) {
-				case '0':
-					$this->response["codigo_respuesta"] = 0;
-					$this->response["mensaje"] = "Ok";
-					$this->response["listaobjetos"] = $objeto;
-					break;				
-				case '-1':
-					$this->response["codigo_respuesta"] = -1;
-					$this->response["mensaje"] = $mensaje;
-					break;
-			}
-		}
-
-		public function ObtenerResponse(){
-			//Aqui se retorna la respuesta
-		}
-
-		public function ExportarJson($nombreArchivo){			
-			file_put_contents($this->rutatemp .$nombreArchivo, json_encode($this->response), FILE_APPEND | LOCK_EX);
+		switch ($codigo) {
+			case '0':
+				$this->response["codigo_respuesta"] = 0;
+				$this->response["mensaje"] = "Ok";
+				$this->response["listaobjetos"] = $objeto;
+			break;				
+			case '-1':
+				$this->response["codigo_respuesta"] = -1;
+				$this->response["mensaje"] = $mensaje;
+			break;
 		}
 	}
-?>
+
+	/**
+	 * Get json in array format.
+	 * 
+	 * @return array
+	 */
+	public function ObtenerResponse(): array
+	{
+		return [
+			[
+			  "tipo" => "carro",
+			  "tamanio" => "Grande",
+			  "color" => "Green"
+			],
+			[
+			  "tipo" => "moto",
+			  "tamanio" => "mediana",
+			  "color" => "Blue"
+			],
+			[
+			  "tipo" => "bicicleta",
+			  "tamanio" => "chica",
+			  "color" => "Green"
+			],
+			[
+			  "tipo" => "avion",
+			  "tamanio" => "grande",
+			  "color" => "yellow"
+			],
+			[
+			  "tipo" => "lancha",
+			  "tamanio" => "grande",
+			  "color" => "Red"
+			],
+			[
+			  "tipo" => "moto",
+			  "tamanio" => "mediano",
+			  "color" => "Red"
+			],
+		];
+	}
+
+	/**
+	 * Export file .json
+	 * 
+	 * @param string $nombreArchivo
+	 * @return void
+	 */
+	public function ExportarJson($nombreArchivo){			
+		file_put_contents($this->rutatemp .$nombreArchivo, json_encode($this->response), FILE_APPEND | LOCK_EX);
+	}
+}
